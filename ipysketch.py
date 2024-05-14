5d7c8e984fe777efd813baaf0b66215a141c1e55
Matthew Taylor <100451342+MatthewAndreTaylor@users.noreply.github.com>
2024-05-14 01:22:20 -0400

    Added bucket fill tool

--- ipysketch.py
+++ ipysketch.py
@@ -43,19 +43,35 @@ class Sketch:
         global response_received
         response_received = threading.Event()
         run()
+        html_code = """
+        <style>
+        #toggleFill {
+            background-color: #f0f0f0;
+            border: 1px solid #999999;
+        }
 
-        html_code = f"""<canvas id="canvas" width="{width}" height="{height}" style="border: 2px solid black;"></canvas>"""
+        #toggleFill.active {
+            background-color: #ffffff;
+            border: 1px solid #000000;
+        }
+        </style>
+        """
+
+        html_code += f"""<canvas id="canvas" width="{width}" height="{height}" style="border: 2px solid black;"></canvas>"""
         html_code += """
+        <button id="toggleFill" onclick="toggleFill()">bucket</button>
         <button onclick="clearCanvas()">clear</button>
         <input id="color" type="color" value="#000000"/>
         <script>
-        var canvas = document.getElementById('canvas');
-        var colorInput = document.getElementById('color');
-        var ctx = canvas.getContext('2d');
+        var canvas = document.getElementById("canvas");
+        var colorInput = document.getElementById("color");
+        var toggleFillButton = document.getElementById("toggleFill");
+        var ctx = canvas.getContext("2d");
         ctx.lineWidth = 4;
         ctx.lineJoin = "round";
         var drawing = false;
-        var mouse = {x: 0, y: 0};
+        var mouse = { x: 0, y: 0 };
+        var fillMode = false;
         
         fetch('http://localhost:5000', {
             method: 'POST',
@@ -65,22 +81,41 @@ class Sketch:
             body: canvas.toDataURL()
         })
 
-        canvas.addEventListener('mousedown', (e) => {
-            mouse = {x: e.offsetX, y: e.offsetY};
+        function toggleFill() {
+            fillMode = !fillMode;
+            toggleFillButton.classList.toggle("active", fillMode);
+        }
+
+        function hexToRgb(hex) {
+            return {
+            r: parseInt(hex.substring(1, 3), 16),
+            g: parseInt(hex.substring(3, 5), 16),
+            b: parseInt(hex.substring(5, 7), 16),
+            a: 255
+            };
+        }
+
+        canvas.addEventListener("mousedown", function(e) {
+            if (fillMode) {
+            floodFill(hexToRgb(colorInput.value), e.offsetX, e.offsetY);
+            return;
+            }
+
+            mouse = { x: e.offsetX, y: e.offsetY };
             drawing = true;
         });
 
-        canvas.addEventListener('mousemove', (e) => {
+        canvas.addEventListener("mousemove", function(e) {
             if (drawing) {
-                drawLine(ctx, mouse.x, mouse.y, e.offsetX, e.offsetY);
-                mouse = {x: e.offsetX, y: e.offsetY};
+            drawLine(ctx, mouse.x, mouse.y, e.offsetX, e.offsetY);
+            mouse = { x: e.offsetX, y: e.offsetY };
             }
         });
 
-        canvas.addEventListener('mouseup', (e) => {
+        canvas.addEventListener("mouseup", function(e) {
             if (drawing) {
-                drawLine(ctx, mouse.x, mouse.y, e.offsetX, e.offsetY);
-                drawing = false;
+            drawLine(ctx, mouse.x, mouse.y, e.offsetX, e.offsetY);
+            drawing = false;
             }
             
             fetch('http://localhost:5000', {
@@ -103,6 +138,61 @@ class Sketch:
 
         function clearCanvas() {
             ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        }
+        
+        function floodFill(color, x, y) {
+            var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            const { width, height, data } = imgData;
+            if (pixelColMatch(x, y, color, data, width)) return;
+
+            const stack = [[x, y]];
+            let baseIdx = (width * y + x) * 4;
+            const oColor = {
+            r: data[baseIdx],
+            g: data[baseIdx + 1],
+            b: data[baseIdx + 2],
+            a: data[baseIdx + 3],
+            };
+
+            while (stack.length) {
+            var [cx, cy] = stack.pop();
+            const move = (dx, dy) => {
+                let nx = cx + dx;
+                let ny = cy + dy;
+                while (
+                ny >= 0 &&
+                ny < height &&
+                pixelColMatch(nx, ny, oColor, data, width)
+                ) {
+                setPixelCol(nx, ny, color, data, width);
+                stack.push([nx, ny]);
+                ny += dy;
+                }
+            };
+            move(0, 1);
+            move(0, -1);
+            move(-1, 0);
+            move(1, 0);
+            }
+            ctx.putImageData(imgData, 0, 0);
+        }
+
+        function pixelColMatch(x, y, color, data, width) {
+            var baseIdx = (width * y + x) * 4;
+            return (
+            data[baseIdx] === color.r &&
+            data[baseIdx + 1] === color.g &&
+            data[baseIdx + 2] === color.b &&
+            data[baseIdx + 3] === color.a
+            );
+        }
+
+        function setPixelCol(x, y, color, data, width) {
+            var baseIdx = (width * y + x) * 4;
+            data[baseIdx] = color.r & 0xff;
+            data[baseIdx + 1] = color.g & 0xff;
+            data[baseIdx + 2] = color.b & 0xff;
+            data[baseIdx + 3] = color.a & 0xff;
         }
         </script>
         """
